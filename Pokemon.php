95c505655506f4fd89e8ffde587e42a66fe37c65
Jis van Overschot <99040091@mydavinci.nl>
2018-03-20 11:35:04 +0100

    Added methods for the attack logic.

--- Pokemon.php
+++ Pokemon.php
@@ -10,17 +10,42 @@ class Pokemon {
 	public $resistance;
 
 	public function __construct($name, $energyType, $hitpoints, $attacks, $weakness, $resistance)
-    {
-        $this->name = $name;
-        $this->energyType = $energyType;
-        $this->hitpoints = $hitpoints;
-        $this->health = $hitpoints;
-        $this->attacks = $attacks;
-        $this->weakness = $weakness;
-        $this->resistance = $resistance;
-    }
+	{
+		$this->name = $name;
+		$this->energyType = $energyType;
+		$this->hitpoints = $hitpoints;
+		$this->health = $hitpoints;
+		$this->attacks = $attacks;
+		$this->weakness = $weakness;
+		$this->resistance = $resistance;
+	}
 
-    public function __toString()
+	public function doAttack($attack, $pokemon)
+	{
+		if ($this->name != $pokemon) {
+			for ($i=0; $i < count($this->attacks); $i++) {
+				if ($this->attacks[$i]->name == $attack) {
+					print_r($this->name . ' attacks ' . $pokemon->name . ' with ' . $attack . ' doing ' . $this->attacks[$i]->demage . ' damage.<br>');
+					$attackDamage = $this->attacks[$i]->demage;
+
+					if ($pokemon->hitpoints < $attackDamage) {
+						$pokemon->getDamage(0);
+					} else {
+						$pokemon->getDamage($pokemon->hitpoints - $attackDamage);
+					}
+				}
+			}
+		}
+		
+	}
+
+	public function getDamage($damage)
+	{
+		
+		$this->hitpoints = $damage;
+	}
+
+	public function __toString()
 	{
 		return json_encode($this);
 	}
--- index.php
+++ index.php
@@ -4,4 +4,20 @@ spl_autoload_register(function ($class_name) {
 });
 
 //print_r('<pre>'. new Pikachu('Pika') . '</pre>');
-print_r('<pre>'. new Charmeleon('Char') . '</pre>');
+//print_r('<pre>'. new Charmeleon('Char') . '</pre>');
+
+$pika = new Pikachu('Pika');
+$char = new Charmeleon('Char');
+
+print_r($pika->name .' HP '. $pika->hitpoints .'<br>');
+print_r($char->name .' HP '. $pika->hitpoints .'<br><br>');
+
+$pika->doAttack('Electric Ring', $char);
+
+print_r($pika->name .' HP '. $pika->hitpoints .'<br>');
+print_r($char->name .' HP '. $char->hitpoints .'<br><br>');
+
+$char->doAttack('Flare', $pika);
+
+print_r($pika->name .' HP '. $pika->hitpoints .'<br>');
+print_r($char->name .' HP '. $char->hitpoints .'<br><br>');
